11abd96fc6ceb0a9c627ab1c457e707224e41abb
cksrlcks <chanki.kim89@gmail.com>
2020-03-02 09:51:48 +0900

    install babel, polyfill

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import { ApolloProvider } from '@apollo/react-hooks';
 import client from './apollo';
 import './css/reset.css'
