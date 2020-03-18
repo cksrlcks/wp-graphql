9003ad5d9f1655b3eb1c80f969d0c1be31e95ad6
cksrlcks <chanki.kim89@gmail.com>
2020-03-18 19:19:28 +0900

    code highlight

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import '../css/post.scss';
@@ -7,12 +7,16 @@ import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { css } from "@emotion/core";
 import { BeatLoader } from "react-spinners";
+import parse, {domToReact} from 'html-react-parser';
+import {PostCode} from '../components/PostCode';
 
 const override = css`
     margin:0 auto;
     text-align:center;
 `
 const formatDate = date => new Date(date).toDateString();
+
+
 
 const GET_SINGLE_POST = gql`
     query GetSinglePost($id: ID!){
@@ -41,13 +45,33 @@ const Post = ({ match, history }) => {
     const { loading, error, data } = useQuery(GET_SINGLE_POST, {
         variables: { id: POST_ID }
     });
-    const content = () => {
-        return { __html: data?.post.content }
+    const content = data?.post.content  ;
+
+    const replaceCode = node => {
+        if (node.name === 'pre') {
+          return node.children.length > 0 && <PostCode language={getLanguage(node)}>{domToReact(getCode(node))}</PostCode>;
+        }
+      };
+    
+    const getLanguage = node => {
+    if (node.attribs.class != null) {
+        console.log(node.attribs.class);
+        return node.attribs.class;
+        
     }
+    return null;
+    };
+    
+    const getCode = node => {
+    if (node.children.length > 0 && node.children[0].name === 'code') {
+        return node.children[0].children;
+    } else {
+        return node.children;
+    }
+    };
 
     return (
         < div className="post_wrap" >
-
             {error ? <p className="error_message">포스트를 불러오는데 실패했습니다</p> :
                 loading ? <BeatLoader
                     size={10}
@@ -61,9 +85,13 @@ const Post = ({ match, history }) => {
                             <p className="date"><FontAwesomeIcon icon={faCalendar} className="post_icon" />{formatDate(data?.post.date)}</p>
                             <p className="writer"><FontAwesomeIcon icon={faPen} className="post_icon" />{data?.post.author.name}</p>
                         </div>
-                        <div className="content" dangerouslySetInnerHTML={content()}></div>
+                        
+                        {/* <div className="content" dangerouslySetInnerHTML={content()}></div> */}
+    <div className="content">{parse(content, {replace: replaceCode})}</div>
                     </>
             }
+
+            
         </div >
     )
 
