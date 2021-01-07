340bdd5d6d685be5ad7d549020dc13dcfb92883e
cksrlcks <chanki.kim89@gmail.com>
2021-01-07 09:31:09 +0900

    주소변경

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: "https://batdog.kr/graphql"
+    uri: "http://cksrlcks.cafe24.com/wp/graphql"
 })
 
 export default client;
